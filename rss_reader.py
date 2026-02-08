fde59ba6711fc8695ed356035ba3e3a85c7e443f
Farah <akirdelouafifarah@gmail.com>
2026-02-08 14:54:56 +0100

    maj rss_reader.py

--- rss_reader.py
+++ rss_reader.py
@@ -1,3 +1,14 @@
 # ce fichier servira à écrire une fonction qui lira des fichiers xml 
 # en retournant le texte et les métadonnées des items du flux RSS
 
+def module_re() :
+	pass
+
+def module_etree():
+	pass
+
+def module_feedparser():
+	pass
+
+def main():
+	pass
