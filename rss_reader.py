9c0902236dfe02886d58b6b55e3bf71621048f1e
Ezd13 <ozenhulya72@gmail.com>
2026-02-16 23:42:27 +0100

    Ex2: traitement arborescence avec pathlib.glob

--- rss_reader.py
+++ rss_reader.py
@@ -2,8 +2,9 @@ import xml.etree.ElementTree as ET
 import os
 import re
 import argparse
+from pathlib import Path
 
-#Récupère le texte d'une balise.Retourne une chaîne vide si la balise est absente.
+#Récupère le texte d'une balise. Retourne une chaîne vide si la balise est absente.
 def get_text(parent, tag):
     elem = parent.find(tag)
     if elem is not None and elem.text:
@@ -12,13 +13,12 @@ def get_text(parent, tag):
 
 #Traite UN fichier RSS XML Retourne une liste de dictionnaires (articles)
     
-def module_etree(chemin_fichier):   
+def module_etree(chemin_fichier):
     articles = []
 
     try:
         tree = ET.parse(chemin_fichier)
     except ET.ParseError:
-        print(f"Fichier XML invalide : {chemin_fichier}")
         return articles
 
     root = tree.getroot()
@@ -27,11 +27,13 @@ def module_etree(chemin_fichier):
     if channel is None:
         return articles
 
+
     for item in channel.findall("item"):
 
         raw_content = get_text(item, "description")
         clean_content = re.sub(r"<[^>]+>", "", raw_content)
 
+        
         article = {
             "id": get_text(item, "guid") or get_text(item, "link"),
             "source": os.path.basename(chemin_fichier),
@@ -50,12 +52,19 @@ def module_etree(chemin_fichier):
     return articles
 
 
-def main(chemin_fichier):
-    articles = module_etree(chemin_fichier)
+def main(dossier):
+    tous_les_articles = []
+    chemin = Path(dossier)
 
-    print("Nombre total d'articles :", len(articles))
+    # Parcours récursif des fichiers XML
+    for fichier in chemin.glob("**/*.xml"):
+    	print("Trouvé :", fichier)
+    	articles = module_etree(fichier)
+    	tous_les_articles.extend(articles)
 
-    for i, article in enumerate(articles, start=1):
+    print("Nombre total d'articles :", len(tous_les_articles))
+    
+    for i, article in enumerate(tous_les_articles, start=1):
         print(f"\nARTICLE {i}")
         print("-" * 40)
         for cle, valeur in article.items():
@@ -67,11 +76,11 @@ if __name__ == "__main__":
         description="Extraction des métadonnées d'un flux RSS"
     )
     parser.add_argument(
-        "fichier_xml",
-        help="Chemin vers le fichier XML RSS"
+        "dossier",
+        help="Chemin vers le dossier contenant les fichiers XML"
     )
 
     args = parser.parse_args()
 
-    main(args.fichier_xml)
+    main(args.dossier)
 
