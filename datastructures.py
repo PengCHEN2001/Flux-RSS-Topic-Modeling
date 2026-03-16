32dfb6b42e2949b94a59b7f46de1d8f4577da496
oushan24 <annushmarina@gmail.com>
2026-03-16 11:50:15 +0100

    maj datastructures.py

--- datastructures.py
+++ datastructures.py
@@ -18,25 +18,26 @@ class Article:
 
 def save_pickle(corpus: list[Article], output_file: Path) -> None:
     #Sauvegarde la liste d'articles au format binaire pickle.
-    with open(output_file, 'wb') as f:
+    with open(output_file, 'wb') as f: #wb : write binary
         pickle.dump(corpus, f)
 
 def load_pickle(input_file: Path) -> list[Article]:
     #Charge et retourne une liste d'articles depuis un fichier pickle.
-    with open(input_file, 'rb') as f:
+    with open(input_file, 'rb') as f: #rb : read binary
         return pickle.load(f)
 
 
 #Mis à jour du main pour utiliser les fonctions de l'exercice 2.
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convertir un corpus d'un format à un autre.")
     
     # On utilise type=Path pour que argparse convertisse directement les strings en objets Path
-    parser.add_argument("input_file", type=Path, help="Chemin du fichier d'entrée")
-    parser.add_argument("output_file", type=Path, help="Chemin du fichier de sortie")
+    parser.add_argument("input_file", type=Path, help="Fichier source")
+    parser.add_argument("output_file", type=Path, help="Fichier cible")
     
-    parser.add_argument("--format-in", choices=["xml", "json", "pickle"], required=True, help="Format du fichier d'entrée")
-    parser.add_argument("--format-out", choices=["xml", "json", "pickle"], required=True, help="Format du fichier de sortie")
+    parser.add_argument("--format-in", choices=["xml", "json", "pickle"], required=True, help="Format du fichier source")
+    parser.add_argument("--format-out", choices=["xml", "json", "pickle"], required=True, help="Format du fichier cible")
 
     args = parser.parse_args()
 
